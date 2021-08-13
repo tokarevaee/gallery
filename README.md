949f0ad763bde430b0b3b26a2649237872287edb
Ekaterina Tokareva [CPA] <e.tokareva@alfaleads.net>
2021-08-13 10:12:07 +0300

    fix(refactoring FormAddImg)

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-# ecwid
 
 ## Project setup
 ```
--- src/components/FormAddImg.vue
+++ src/components/FormAddImg.vue
@@ -2,7 +2,7 @@
   <div>
     <form  @submit.prevent="submitFileHandler" class="d-flex">
       <div class="input__wrapper">
-        <input  id="input__file" type="file" ref="fileImage" class="input input__file" v-on:change="handleFileUpload()" placeholder="Вставьте ссылку на изображение или на список изображений">
+        <input  id="input__file" type="file" multiple ref="fileImage" class="input input__file" v-on:change="handleFileUpload()" placeholder="Вставьте ссылку на изображение или на список изображений">
         <label for="input__file" class="input__file-button">
           <span class="input__file-icon-wrapper"><img class="input__file-icon" src="@/assets/add.svg" alt="Выбрать файл" width="25"></span>
           <span class="input__file-button-text"> {{ inputTextButton }} </span>
@@ -16,6 +16,50 @@
 </template>
 
 <script>
+function readFileAsync(file) {
+  return new Promise((resolve, reject) => {
+    let reader = new FileReader();
+    const fileConstructor = {};
+    reader.onload = () => {
+      let image = new Image();
+      image.src = reader.result;
+      fileConstructor.url = image.src;
+      image.onload = function () {
+        fileConstructor.width = image.width;
+        fileConstructor.height = image.height;
+
+        resolve(fileConstructor);
+      };
+    };
+    reader.onerror = reject;
+    reader.readAsDataURL(file);
+  });
+}
+
+function readFileJsonAsync(file) {
+  return new Promise((resolve) => {
+    let fileToJSON = function (file) {
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader()
+        reader.onload = () => {
+          try {
+            resolve(JSON.parse(reader.result));
+          } catch (err) {
+            reject();
+          }
+        }
+        reader.readAsText(file);
+      })
+    };
+    fileToJSON(file).then((response) => {
+      const { galleryImages } = response;
+      resolve(galleryImages);
+    }).catch((error) => {
+      console.log(error)
+    })
+  });
+}
+
 export default {
   name: "FormAddImg",
   data() {
@@ -23,19 +67,18 @@ export default {
       form: {
         file: '',
       },
-      file: null,
-      images: [],
+      files: [],
       isLoading: false,
     };
   },
   computed: {
     inputTextButton() {
-      return this.file ? 'Файл выбран, нажмите "Загрузить"' : 'Выберите файл .json'
+      return this.file ? 'Файл выбран, нажмите "Загрузить"' : 'Выберите изображения или файлы .json'
     }
   },
   methods: {
     handleFileUpload() {
-        this.file = this.$refs.fileImage.files[0];
+        this.files = this.$refs.fileImage.files;
     },
 
     async fileToJSON(file) {
@@ -47,21 +90,27 @@ export default {
       })
     },
     async submitFileHandler() {
-      if(this.file) {
-        if(this.file.type === "application/json") {
-          await this.fileToJSON(this.file).then((response) =>{
-            const { galleryImages } = response;
-            this.form.file = galleryImages
-            this.$emit('galleryImages', this.form.file);
-            this.form.file = ''
-            this.file = null
-          }).catch((error) => {
-            console.log(error)
-          })
-        } else if (this.file.type === "image/png" || this.file.type === "image/jpeg"){
-          console.log('error')
+      const fileToImgArr = async () => {
+        let uploadFiles = [];
+        for await (let file of this.files) {
+          if (file.type === "image/png" || file.type === "image/jpeg") {
+            const f = await readFileAsync(file);
+            uploadFiles.push(f);
+          } else if (file.type === "application/json") {
+            const f = await readFileJsonAsync(file);
+            uploadFiles= [...uploadFiles, ...f];
+          }
         }
-      }
+        return uploadFiles;
+      };
+      fileToImgArr()
+          .then((response) => {
+            this.$emit("galleryImages", response);
+            this.files = [];
+            this.uploadFilesArray = [];
+          }).catch((error) => {
+        console.log(error);
+      });
     },
   },
 
